32f991766b8f679a70427231d0ded2c83bc9e803
Poojan Patel <poojan.patel@biofourmis.com>
2023-04-27 18:09:45 +0530

    Added initial setup and db connection

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Button from '@/components/Button'
+import { FC } from 'react'
+
+interface pageProps {
+
+}
+
+const page: FC<pageProps> = ({ }) => {
+    return <Button >click here</Button>
+}
+
+export default page
