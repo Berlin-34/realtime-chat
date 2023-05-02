dbe80558742789eb29dafd38fe28b1ee8554a83c
Poojan Patel <poojan.patel@biofourmis.com>
2023-05-02 13:23:14 +0530

    Finished project

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,10 +1,12 @@
 import { getFriendsByUserId } from '@/app/helpers/get-friends-by-user-id'
 import { fetchRedis } from '@/app/helpers/redis'
 import FriendRequestSidebarOptions from '@/components/FriendRequestSidebarOptions'
-import { Icon, Icons } from '@/components/Icons'
+import { Icons } from '@/components/Icons'
+import MobileChatLayout from '@/components/MobileChatLayout'
 import SideBarChatList from '@/components/SideBarChatList'
 import SignOutButton from '@/components/SignOutButton'
 import { authOptions } from '@/lib/auth'
+import { SidebarOption } from '@/types/typings'
 import { getServerSession } from 'next-auth'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -13,13 +15,6 @@ import { ReactNode } from 'react'
 
 interface LayoutProps {
   children: ReactNode
-}
-
-interface SidebarOption {
-  id: number
-  name: string
-  href: string
-  Icon: Icon
 }
 
 const sidebarOptions: SidebarOption[] = [
@@ -45,7 +40,15 @@ const Layout = async ({ children }: LayoutProps) => {
   ).length
   return (
     <div className="w-full flex h-screen">
-      <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+      <div className="md:hidden">
+        <MobileChatLayout
+          friends={friends}
+          session={session}
+          unseenRequestCount={unseenRequestCount}
+          sidebarOptions={sidebarOptions}
+        />
+      </div>
+      <div className="hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
         <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
           <Icons.Logo className="h-8 w-auto text-indigo-600" />
         </Link>
